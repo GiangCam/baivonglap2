592a4cabfb5941163faa802bd10f7e72941fbd10
ADMIN <ADMIN@LAB1-MAY10>
2024-03-04 09:47:42 +0700

    Add project files.

---
+++ baivonglap2/baivonglap2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int  sodau, socuoi;
+	cout << "nhap dau:";
+	cin >> sodau;
+	cout << "nhap cuoi:";
+	cin >> socuoi;
+
+	cout << "ban vong lap\n";
+	while (sodau <= socuoi)
+	{
+		cout << sodau << "\n";
+		sodau++;
+
+	}
+	cout << "vong lap while\n";
+	
+	do {
+		cout << sodau << " ";
+		sodau++;
+		while (())
+
+	}
+
+	return 0;
+}
